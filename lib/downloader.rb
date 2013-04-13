02d038f8390b47a009d49e5c978df465125a460b
jelf <begdory4@gmail.com>
2013-04-13 12:33:49 +0400

    Rev1015 is finished. Added debug configuration

--- lib/downloader.rb
+++ lib/downloader.rb
@@ -22,8 +22,9 @@ class Downloader
     @audios        = audios
     @namer_finished= false
     
-    @audios_passed = 0
+    @audios_passed = 0 #syncronizes with queue_mutex
     @audios_downloaded = 0
+    @audios_downloaded_mutex = Mutex.new
     @max_audios    = audios.length
 
     @ROOT = Pathname.new $cfg['ROOT']
@@ -34,6 +35,7 @@ class Downloader
       res = (@namer.lookup2 audio) << audio.url
       @queue_mutex.synchronize do
         @queue << res
+        @audios_passed += 1
       end
     end
     thread
@@ -58,17 +60,20 @@ class Downloader
       folder = @ROOT+folder
       Downloader.es folder
       `#{@WGET} -c -O "#{(folder+title).to_path}.mp3" #{url}`
+      @audios_downloaded_mutex.synchronize do
+        @audios_downloaded += 1
+      end
     end
     thread 
   end
   def wget_thread_manager
     while(!@namer_finished or !@queue.empty?) do
       @dl_threads=@dl_threads.find_all {|t| t.alive?}      
-      if (@dl_threads.length < MAX_DL_THREADS)
-        nthr = new_wget_thread(queue.shift)
+      if ((@dl_threads.length < MAX_DL_THREADS) && !queue.empty?)
         @queue_mutex.synchronize do 
-          @dl_threads << nthr 
+          @ntpar = queue.shift
         end
+        @dl_threads << new_wget_thread(@ntpar)
       else 
         sleep 0.01
       end
@@ -80,7 +85,11 @@ class Downloader
   def start
     que = Thread.new { namer_thread_manager }
     dle = Thread.new {  wget_thread_manager }
-    que.join
-    dle.join
+    while( que.status || dle.status ) do
+      puts "\nPassed:  #{@audios_passed}(#{@audios_passed*100/@max_audios}%), Downloaded: #{@audios_downloaded}(#{@audios_downloaded*100/@max_audios}%), Total: #{@max_audios}"
+      sleep 1
+    end
+    p que.status
+    p dle.status
   end
 end
